5963029dac5d6db01bc470e41554b27b17d1b73d
Manuel J. Perez <manuel@xaibot.com>
2024-09-18 02:19:33 +0200

    fixup! Add logic for creating and listing events

--- db/migrate/20240913164239_create_events.rb
+++ db/migrate/20240913164239_create_events.rb
@@ -8,6 +8,7 @@ class CreateEvents < ActiveRecord::Migration[7.2]
       t.string :address, null: false
       t.datetime :starts_at, null: false
       t.integer :max_tickets, null: false
+      t.integer :booked_tickets, null: false, default: 0
 
       t.timestamps
     end
